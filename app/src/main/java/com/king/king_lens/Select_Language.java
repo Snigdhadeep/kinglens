c04f6cd16b428bdb9af83dfacb4266f0f62f6f36
snigdhadeep <snigdhadeep93@gmail.com>
2016-11-05 00:37:16 -0700

    language screen

---
+++ app/src/main/java/com/king/king_lens/Select_Language.java
@@ -0,0 +1,13 @@
+package com.king.king_lens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Select_Language extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select__language);
+    }
+}
